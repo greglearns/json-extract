588a362f04e254b755d1070cc7d42912c99dfc5f
Greg Edwards <greg@greglearns.com>
2013-09-09 16:04:36 -0600

    Add support for nested keys.
    something.nested will work for nested objects.
    an_array.3 works the same as an_array[3].

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 var assert = require('assert')
 
 module.exports = function (args) {
-	validateArgs(args)
-	return JSON.parse(args.json)[args.key]
+  validateArgs(args)
+  var current = JSON.parse(args.json)
+
+  args.key.split('.').forEach(function(key) {
+    current = current[key]
+  })
+
+  return current
 }
 
 function validateArgs (args) {
-	assert.ok(typeof args === 'object', 'You must pass an object with keys [json, key]')
-	assert.ok(args.hasOwnProperty("json"), 'You must pass in an object that has a "json" key.')
-	assert.ok(args.hasOwnProperty("key"), 'You must pass in an object that has a "key" key.')
+  assert.ok(typeof args === 'object', 'You must pass an object with keys [json, key]')
+  assert.ok(args.hasOwnProperty("json"), 'You must pass in an object that has a "json" key.')
+  assert.ok(args.hasOwnProperty("key"), 'You must pass in an object that has a "key" key.')
 }
 
--- test/extract-test.js
+++ test/extract-test.js
@@ -1,6 +1,9 @@
 require('./test-helper')
 var expect = require('chai').expect
 var extract = require('../')
+var fs = require('fs')
+var path = require('path')
+var fixture = fs.readFileSync(path.join(__dirname, 'fixture/good.json'), 'utf8')
 
 describe('extract JSON values from a JSON string', function () {
   var key = "the_key"
@@ -35,6 +38,32 @@ describe('extract JSON values from a JSON string', function () {
 
   it('throw an error if required arg "key" is missing', function () {
     expect(callExtractWithoutRequiredArg('key')).to.throw(Error)
+  })
+
+  describe('paths', function() {
+
+    describe('accepts', function() {
+
+      it('nested.value', function() {
+        extractParameters.key = 'nested.value'
+        extractParameters.json = fixture
+        expect(extract(extractParameters)).to.equal('I am a nested value')
+      })
+
+      it('an_array.0: accesses the first item in an arrray ', function() {
+        extractParameters.key = 'an_array.0'
+        extractParameters.json = fixture
+        expect(extract(extractParameters)).to.equal('an')
+      })
+
+      it('nested.nestedAgain.an_array.1.totally: accesses nested objects inside of nested arrays', function() {
+        extractParameters.key = 'nested.nestedAgain.an_array.1.totally'
+        extractParameters.json = fixture
+        expect(extract(extractParameters)).to.equal('nested dude')
+      })
+
+    })
+
   })
 
   function callExtractWithoutRequiredArg (arg) {
